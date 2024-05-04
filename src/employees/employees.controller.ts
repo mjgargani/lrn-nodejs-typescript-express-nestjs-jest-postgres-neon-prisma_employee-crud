6b8c42336a94752cecd151be03d57f6ec47a7f7d
Rodrigo Gargani Oliveira <mjgargani@gmail.com>
2024-05-04 16:01:55 -0300

    lesson06

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -7,25 +7,36 @@ import {
   Param,
   Delete,
   Query,
+  Ip,
 } from '@nestjs/common';
 import { EmployeesService } from './employees.service';
 import { Prisma } from '@prisma/client';
 import { UserRole } from 'src/users/dto/create-user.dto';
+import { Throttle, SkipThrottle } from '@nestjs/throttler';
+import { LoggerService } from 'src/logger/logger.service';
 
+@SkipThrottle()
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
+  private readonly logger = new LoggerService(EmployeesController.name);
 
   @Post()
   create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
     return this.employeesService.create(createEmployeeDto);
   }
 
+  @SkipThrottle({ default: false })
   @Get()
-  findAll(@Query('role') role?: UserRole) {
+  findAll(@Ip() ip: string, @Query('role') role?: UserRole) {
+    this.logger.log(
+      `Request for ALL Employees\t${ip}`,
+      EmployeesController.name,
+    );
     return this.employeesService.findAll(role);
   }
 
+  @Throttle({ short: { ttl: 1000, limit: 1 } })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.employeesService.findOne(+id);
